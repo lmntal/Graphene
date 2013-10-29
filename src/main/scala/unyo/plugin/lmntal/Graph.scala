9969b4de69b5d9da557485eb1708881038d24f45
Naoki YAGUCHI <murooka.1129.x50@gmail.com>
2013-10-29 22:48:28 +0900

    Use ID type

--- src/main/scala/unyo/plugin/lmntal/Graph.scala
+++ src/main/scala/unyo/plugin/lmntal/Graph.scala
@@ -6,8 +6,12 @@ trait Edge {
 
 import collection.mutable.Set
 
+trait ID
+case class IntID(value: Int) extends ID
+case class DataAtomID(id: ID, port: Int) extends ID
+
 class Mem(
-  val id: Int,
+  val id: ID,
   val name: String,
   val parent: Mem,
   val atoms: Set[Atom],
@@ -16,7 +20,13 @@ class Mem(
   def allAtoms: Set[Atom] = atoms ++ mems.flatMap(_.allAtoms)
   def allMems: Set[Mem] = mems ++ mems.flatMap(_.allMems)
 }
-class Atom(val id: Int, val name: String, val parent: Mem, val arity: Int, val edges: Array[Edge]) {
+class Atom(
+  val id: ID,
+  val name: String,
+  val parent: Mem,
+  val arity: Int,
+  val edges: Array[Edge]
+) {
   def buddyAt(i: Int): Atom = {
     val buddy = actualBuddyAt(i)
     if (buddy.isProxy) buddy.actualBuddyAt(0).buddyAt(1) else buddy
@@ -47,7 +57,7 @@ object Mem {
   var idSeed = 65535
   def nextID = { idSeed += 1; idSeed }
 
-  private def configureLinks(mem: Mem, map: Map[Int, Atom]) {
+  private def configureLinks(mem: Mem, map: Map[ID, Atom]) {
     val newAtoms = ArrayBuffer.empty[Atom]
     for (n <- mem.atoms) {
       val edges = n.edges
@@ -55,9 +65,9 @@ object Mem {
         edges(i) match {
           case Raw(attr, data) => {
             if ((attr & 0x80) == 0) {
-              edges(i) = Link(map(data.toInt))
+              edges(i) = Link(map(IntID(data.toInt)))
             } else {
-              val node = new Atom(nextID, data, mem, 1, Array(Link(n)))
+              val node = new Atom(IntID(nextID), data, mem, 1, Array(Link(n)))
               edges(i) = Link(node)
               newAtoms += node
             }
@@ -70,8 +80,8 @@ object Mem {
     mem.atoms ++= newAtoms
   }
 
-  private def atomMapIn(mem: Mem): Map[Int, Atom] = {
-    val b = Map.newBuilder[Int, Atom]
+  private def atomMapIn(mem: Mem): Map[ID, Atom] = {
+    val b = Map.newBuilder[ID, Atom]
 
     for (n <- mem.atoms) b += n.id -> n
     for (n <- mem.mems) b ++= atomMapIn(n)
@@ -84,7 +94,7 @@ object Mem {
     val JString(name) = json \ "name"
     val JArray(atoms) = json \ "atoms"
     val JArray(mems) = json \ "membranes"
-    val mem = new Mem(id.toInt, name, parent, Set(), Set())
+    val mem = new Mem(IntID(id.toInt), name, parent, Set(), Set())
     mem.atoms ++= atoms.map(buildAtom(_, mem))
     mem.mems ++= mems.map(buildMem(_, mem))
     mem
@@ -95,7 +105,7 @@ object Mem {
     val JString(name) = json \ "name"
     var JArray(links) = json \ "links"
     if (name == "$in" || name == "$out") links = links.take(2)
-    new Atom(id.toInt, name, parent, links.size, links.map(buildEdge _).toArray)
+    new Atom(IntID(id.toInt), name, parent, links.size, links.map(buildEdge _).toArray)
   }
 
   private[this] val intAttr      = 0x80 | 0x00
@@ -120,7 +130,7 @@ object Mem {
 import unyo.util._
 
 class ViewContext {
-  private val viewNodeFromID = collection.mutable.Map.empty[Int, View]
+  private val viewNodeFromID = collection.mutable.Map.empty[ID, View]
   val r = new util.Random
   def viewOf(node: Atom): View = {
     viewNodeFromID.getOrElseUpdate(node.id, {
