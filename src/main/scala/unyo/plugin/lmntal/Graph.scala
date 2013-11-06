106507d8f8c83da364208d8ae1e1a5d64d3c8e37
Naoki YAGUCHI <murooka.1129.x50@gmail.com>
2013-11-07 01:55:29 +0900

    Add SLIM JSON parser for general graph

--- src/main/scala/unyo/plugin/lmntal/Graph.scala
+++ src/main/scala/unyo/plugin/lmntal/Graph.scala
@@ -35,6 +35,7 @@ class Atom(
 
 case class Attribute(value: Int) {
   def isRef = (value & 0x80) == 0
+  def targetPos = value | 0x7F
   def isData = !isRef
   def isHL = value == 0x8a
 }
@@ -46,6 +47,71 @@ case class Raw(attr: Attribute, data: String) extends Link {
 }
 case class Ref(node: Atom) extends Link
 
+object LMN {
+
+  import org.json4s._
+  import org.json4s.native.JsonMethods._
+  import unyo.model.{Builder,Graph}
+  import unyo.model.Builder.MutableNode
+
+  def fromString(s: String): Graph = {
+    val builder = new Builder
+    buildMem(builder, parse(s), null)
+    builder.build
+  }
+
+  case class IntID(value: Int) extends unyo.model.ID
+  case class DataAtomID(id: unyo.model.ID, pos: Int) extends unyo.model.ID
+
+  def buildMem(builder: Builder, json: JValue, parent: MutableNode) {
+    val JInt(id) = json \ "id"
+    val JString(name) = json \ "name"
+    val JArray(atoms) = json \ "atoms"
+    val JArray(mems) = json \ "membranes"
+
+    val node = if (parent == null) builder.addRoot(IntID(id.toInt), name) else builder.addNode(IntID(id.toInt), name, parent)
+    for (m <-  mems) buildMem( builder, m, node)
+    for (a <- atoms) buildAtom(builder, a, node)
+  }
+
+  def buildAtom(builder: Builder, json: JValue, parent: MutableNode) {
+    val JInt(id) = json \ "id"
+    val JString(name) = json \ "name"
+    var JArray(links) = json \ "links"
+    if (name == "$in" || name == "$out") links = links.take(2)
+
+    val node = builder.addNode(IntID(id.toInt), name, parent)
+    for ((l,i) <- links.zipWithIndex) buildLink(builder, l, parent, node, i)
+  }
+
+  def buildLink(builder: Builder, json: JValue, parent: MutableNode, buddy: MutableNode, pos: Int) {
+    val JInt(attr) = json \ "attr"
+    val data = (json \ "data") match {
+      case JString(s) => s
+      case JDouble(d) => d.toString
+      case JInt(i)    => i.toString
+      case j          => throw new Exception("Unexpected data : " + j.toString)
+    }
+    val attribute = Attribute(attr.toInt)
+    if (attribute.isRef) {
+      (json \ "data") match {
+        case JInt(i) => builder.addEdge(buddy.id, pos, IntID(i.toInt), attribute.targetPos)
+        case j          => throw new Exception("Unexpected data : " + j.toString)
+      }
+    } else {
+      val data = (json \ "data") match {
+        case JString(s) => s
+        case JDouble(d) => d.toString
+        case JInt(i)    => i.toString
+        case j          => throw new Exception("Unexpected data : " + j.toString)
+      }
+      val id = DataAtomID(buddy.id, pos)
+      builder.addNode(id, data, parent)
+      builder.addEdge(buddy.id, pos, id, 0)
+    }
+  }
+
+}
 
 object Mem {
   import org.json4s._
