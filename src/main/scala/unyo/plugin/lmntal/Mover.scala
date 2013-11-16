601cd38333757afa08e98cd9e07f7470635d4e5e
Naoki YAGUCHI <murooka.1129.x50@gmail.com>
2013-11-16 10:50:37 +0900

    Adjust the contractile force

--- src/main/scala/unyo/plugin/lmntal/Mover.scala
+++ src/main/scala/unyo/plugin/lmntal/Mover.scala
@@ -79,13 +79,14 @@ class DefaultMover extends LMNtalPlugin.Mover {
   }
 
   private def forceOfContraction(parent: Node, child: Node): Point = {
-    if (parent.isRoot) {
+    val surplusArea = vctx.viewOf(parent).rect.area - parent.allChildNodes.size * 10000
+    if (parent.isRoot || surplusArea < 100) {
       Point(0, 0)
     } else {
       val parentView = vctx.viewOf(parent)
       val childView = vctx.viewOf(child)
       val d = parentView.rect.center - childView.rect.center
-      d
+      d.unit * math.sqrt(d.abs * math.sqrt(surplusArea)) / 2
     }
   }
 
