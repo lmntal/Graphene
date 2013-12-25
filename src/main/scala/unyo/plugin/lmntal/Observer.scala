cd51607e86f5a96f06b60058387a9b372d1e2e07
Naoki YAGUCHI <murooka.1129.x50@gmail.com>
2013-12-25 18:50:51 +0900

    Refactor event handling

--- src/main/scala/unyo/plugin/lmntal/Observer.scala
+++ src/main/scala/unyo/plugin/lmntal/Observer.scala
@@ -10,45 +10,32 @@ import unyo.model._
 class Observer extends LMNtal.Observer {
 
   import java.awt.event.{KeyEvent}
+  import java.awt.{Point => JPoint}
 
   private def viewOptAt(wp: Point): Option[View] = {
     val graph = LMNtal.source.current
     graph.rootNode.allChildNodes.filter(_.childNodes.isEmpty).find(_.view.rect.contains(wp)).map(_.view)
   }
 
-  var view: View = null
   var isNodeHandlable = false
-  lazy val graphicsContext = unyo.core.gui.MainFrame.instance.mainPanel.graphicsContext
-  var prevPoint: java.awt.Point = null
+  lazy val gctx = unyo.core.gui.MainFrame.instance.mainPanel.graphicsContext
+
+  var view: View = null
+  def viewPressed(p: JPoint): Unit = for (v <- viewOptAt(gctx.worldPointFrom(p))) { view = v; v.fixed = true }
+  def viewDragged(p: JPoint): Unit = if (view != null) view.rect = Rect(gctx.worldPointFrom(p), view.rect.dim)
+  def viewReleased(p: JPoint): Unit = if (view != null) { view.fixed = false; view = null }
+
+  var prevPoint: JPoint = null
+  def screenPressed(p: JPoint): Unit = prevPoint = p
+  def screenDragged(p: JPoint): Unit = if (prevPoint != null) { gctx.moveBy(prevPoint - p); prevPoint = p }
+  def screenReleased(p: JPoint): Unit = prevPoint = null
 
   def listener: Reactions.Reaction = {
-    case MousePressed(_, p, _, _, _) =>
-      if (isNodeHandlable) {
-        val pos = graphicsContext.worldPointFrom(p)
-        for (v <- viewOptAt(pos)) { view = v; v.fixed = true }
-      } else {
-        prevPoint = p
-      }
-    case MouseReleased(_, p, _, _, _) =>
-      if (isNodeHandlable) {
-        view.fixed = false
-        view = null
-      } else {
-        prevPoint = null
-      }
-    case MouseDragged(_, p, _) =>
-      if (isNodeHandlable) {
-        if (view != null) {
-          val wp = graphicsContext.worldPointFrom(p)
-          view.rect = Rect(wp, view.rect.dim)
-        }
-      } else if (prevPoint != null) {
-        graphicsContext.moveBy(prevPoint - p)
-        prevPoint = p
-      }
-    case MouseWheelMoved(_, p, _, rot) => graphicsContext.zoom(math.pow(1.01, rot), p)
-    case KeyPressed(_, key, _, _) => if (key == KeyEvent.VK_Z) isNodeHandlable = true
-    case KeyReleased(_, key, _, _) => if (key == KeyEvent.VK_Z) isNodeHandlable = false
+    case MousePressed(_, p, _, _, _)  => if (isNodeHandlable) viewPressed(p)  else screenPressed(p)
+    case MouseReleased(_, p, _, _, _) => if (isNodeHandlable) viewReleased(p) else screenReleased(p)
+    case MouseDragged(_, p, _)        => if (isNodeHandlable) viewDragged(p)  else screenDragged(p)
+    case MouseWheelMoved(_, p, _, rot) => gctx.zoom(math.pow(1.01, rot), p)
+    case KeyPressed(_, key, _, _) => if (key == KeyEvent.VK_Z) isNodeHandlable = !isNodeHandlable
     case _ =>
   }
 }
