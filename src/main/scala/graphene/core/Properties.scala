b0a390b4909266bf9b75b526607c2d2480b1bd3b
Kazunori Ueda <kazunori.ueda.ku@gmail.com>
2022-05-05 12:55:21 +0900

    merged improvements of suan/graphics

--- src/main/scala/graphene/core/Properties.scala
+++ src/main/scala/graphene/core/Properties.scala
@@ -1,6 +1,6 @@
 package graphene.core
 
-import java.io.{File,FileReader,PrintWriter,FileWriter}
+import java.io.{File, FileReader, PrintWriter, FileWriter}
 import com.typesafe.scalalogging.Logger
 import org.slf4j.LoggerFactory
 
@@ -11,9 +11,17 @@ object Properties {
   def load(filename: String): java.util.Properties = {
     val properties = new java.util.Properties
     try {
-      properties.load(new FileReader(filename))
+      //CHANGED ファイルを.jarのある位置の絶対パスで指定
+      properties.load(new FileReader(graphene.core.Env.rootPath + filename))
     } catch {
-      case e: java.io.FileNotFoundException => logger.info("Cannot find {}. Use default properties", filename)
+      case e: java.io.FileNotFoundException => {
+        try {
+          properties.load(new FileReader(filename))
+        } catch {
+          case e2: java.io.FileNotFoundException => 
+          logger.info("Cannot find {}. Use default properties", filename)
+        }
+      }
     }
     properties
   }
